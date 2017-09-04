ea4d580c797800c1c3d3bf249c961f9dd8d780c5
dukim <dukim@bnpinnovation.com>
2017-09-04 12:11:54 +0900

    수정

--- docker-run.sh
+++ docker-run.sh
@@ -1,1 +1,1 @@
-docker run -d -v /var/run/docker.sock:/var/run/docker.sock -p 8080:8080 -p 50000:50000 jenkins-with-docker-socke
+docker run -d -v /var/run/docker.sock:/var/run/docker.sock -p 49001:8080 -p 50000:50000 bnpdukim/jenkins
